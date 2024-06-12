15a6e2237e4461d3aa5e478ba9c4919dd45f58ca
avoe <inuser027@gmail.com>
2024-06-12 23:58:37 +0400

    Renamed folders and files

---
+++ Python/problem-2.py
@@ -0,0 +1,20 @@
+'''
+2. color = ['aqua 0 255 255', 'black 0 0 0', 'blue 0 0 255', 'fuchsia 255 0
+255', 'gray 128 128 128', 'green 0 128 0', 'lime 0 255 0', 'red 255 0 0',
+'white 255 255 255', 'yellow 255 255 0']
+Proqram eyni anda rəngi və boşluq vasitəsilə onun RGB formatında
+nömrəsini saxlayan verilənlər bazası ilə işləyir.
+Sıfır rəqəmini ("0") üç sıfırla ("000") əvəz edin. Yeni verilənlər bazasını
+ekranda əks etdirin.
+
+Çıxışda alınmalıdır:
+['aqua 000 255 255', 'black 000 000 000', 'blue 000 000 255', 'fuchsia
+255 000 255', 'gray 128 128 128', 'green 000 128 000', 'lime 000 255
+000', 'red 255 000 000', 'white 255 255 255', 'yellow 255 255 000']
+'''
+
+color = ['aqua 0 255 255', 'black 0 0 0', 'blue 0 0 255', 'fuchsia 255 0 255', 'gray 128 128 128', 'green 0 128 0', 'lime 0 255 0', 'red 255 0 0', 'white 255 255 255', 'yellow 255 255 0']
+
+for i in range(len(color)):
+    if '0' in color[i]:
+        
