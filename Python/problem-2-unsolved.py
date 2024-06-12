99f8d5319d6ed82da0b0348d837fbb47cfa14f07
avoe <inuser027@gmail.com>
2024-06-13 00:10:07 +0400

    commit

--- Python/problem-2-unsolved.py
+++ Python/problem-2-unsolved.py
@@ -16,5 +16,6 @@ ekranda əks etdirin.
 color = ['aqua 0 255 255', 'black 0 0 0', 'blue 0 0 255', 'fuchsia 255 0 255', 'gray 128 128 128', 'green 0 128 0', 'lime 0 255 0', 'red 255 0 0', 'white 255 255 255', 'yellow 255 255 0']
 
 for i in range(len(color)):
-    if '0' in color[i]:
-        
+    pass
+
+print(color)
