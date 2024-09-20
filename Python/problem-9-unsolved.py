9ba7eff9c9238986d115a7db934185f0aab0f499
avoe <inuser027@gmail.com>
2024-09-20 19:01:13 +0400

    .

--- Python/problem-9-unsolved.py
+++ Python/problem-9-unsolved.py
@@ -3,19 +3,17 @@ import math
 9. n sayda tam ədəd verilib. Onların içərisində elə iki ədəd tapın ki,
 ƏBOB qiyməti ən böyük olsun.
 
-
 UNSOLVED
 '''
 
-a = int(input('say: '))
-f =[]
-yoxla=[]
-for i in range(a):
-    x = int(input())
-    f.append(x)
-for d in range(len(f)):
-    fd = f[d]
-    for g in range(len-d):
-        yoxla.append(math.gcd(f[d],f[d+g]))
-        print(yoxla, 'test')
-    
+# a = int(input('say: '))
+# f =[]
+# yoxla=[]
+# for i in range(a):
+#     x = int(input())
+#     f.append(x)
+# for d in range(len(f)):
+#     fd = f[d]
+#     for g in range(len(g)-d):
+#         yoxla.append(math.gcd(f[d],f[d+g]))
+#         print(yoxla, 'test')
